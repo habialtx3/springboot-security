04328fbe608e143d16d8e077ff218dd8c9cd2d5d
Muhammad Reza Pahlevi <rezaworku31@gmail.com>
2026-05-25 15:48:50 +0700

    feat : implemented jwt for login

--- src/main/java/com/habialtx3/springboot_security/controller/UserController.java
+++ src/main/java/com/habialtx3/springboot_security/controller/UserController.java
@@ -20,7 +20,6 @@ public class UserController {
 
     @PostMapping("/login")
     public String login(@RequestBody User user) {
-        System.out.println(user);
-        return "Success";
+        return userService.verify(user);
     }
 }
