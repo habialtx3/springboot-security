84d551b12e7a7d16195abeec6b1bd20517aa4564
Muhammad Reza Pahlevi <rezaworku31@gmail.com>
2026-05-25 16:37:11 +0700

    feat : feature jwt verify implementation for login to other method

--- src/main/java/com/habialtx3/springboot_security/config/SecurityConfig.java
+++ src/main/java/com/habialtx3/springboot_security/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +25,9 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     private BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder(10);
 
@@ -38,6 +42,7 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 //        http.formLogin(Customizer.withDefaults());
     }
